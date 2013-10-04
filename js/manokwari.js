70c33e4673ef9d45de7d0888b68f10830636570e
Mohammad Anwari <mdamt@mdamt.net>
2013-10-04 22:01:18 +0700

    Changes: Rearrange how requirejs loads the modules

--- js/manokwari.js
+++ js/manokwari.js
@@ -1,11 +1,17 @@
-requirejs(["js/jq.js", 
-    "moment.min", 
-    "ui",
-    "manokwari/session",
-    ], function(jq, m, ui, d) {
-  $(document).ready(function() {
-    ui.initialize();
-    var session = new Session();
-  });
+requirejs.config({
+  baseUrl: "js",
+  paths: {
+    "ui": "ui",
+    "jquery": "jq",
+  }
 });
 
+requirejs(["jquery", "moment.min"], function($) {
+  requirejs(["ui", "manokwari/session"], function(ui, session) {
+    $(document).ready(function() {
+      ui.initialize();
+      var s = new Session();
+
+    });
+  })
+});
